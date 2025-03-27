d9967189a67e0d8f93b766e714816bc2455e8eaf
JUNGI JEONG <junejjk@gmail.com>
2025-03-28 02:29:37 +0900

    Setting 13

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/JUNKI007.github.io' : '',
-  assetPrefix: isProd ? '/JUNKI007.github.io/' : '',
+  basePath: '/JUNKI007.github.io',
+  assetPrefix: '/JUNKI007.github.io/',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
