076960fd5a137a773f77c57e61d7232a801e42c3
JUNGI JEONG <junejjk@gmail.com>
2025-03-30 20:27:50 +0900

    config changed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/JUNKI007.github.io',
-  assetPrefix: '/JUNKI007.github.io/',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
